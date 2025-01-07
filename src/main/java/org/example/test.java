2f097916f4a4f07ba9aafcd0031a8019e01b8272
Rahul Dev Banjara <rdbanjara07@gmail.com>
2025-01-07 18:45:48 +0545

    Initial commit

---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class test {
+    public String getName(int number){
+        if (number == 1) {
+            return "Rahul";
+        }
+        else{
+            return "Smriti";
+        }
+
+    }
+}
